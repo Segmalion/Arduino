cccc1134498e9b040f12cfc4b53db3e083c0564e
Vladimir Melnychenko <greencapsul@gmail.com>
2019-10-07 02:15:18 +0300

    Фен-Шуй

--- Meteo-WiFi/src/main.cpp
+++ Meteo-WiFi/src/main.cpp
@@ -1,81 +1,85 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include <SPI.h>
-//#include <Adafruit_Sensor.h> --- Оказывается, он не обязателен....
 #include <Adafruit_BME280.h>
-
-#include <ESP8266WiFi.h>
-#include <DNSServer.h>
-#include <ESP8266WebServer.h>
-
-#include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 #include <GyverEncoder.h>
+
+// Оказывается, они не обязателены....
+// #include <ESP8266WiFi.h>
+// #include <DNSServer.h>
+// #include <ESP8266WebServer.h>
+// #include <ESP8266mDNS.h>
+// #include <SPI.h>
+// #include <Adafruit_Sensor.h>
 
 #define sendNM true
 #define debug false                     // вывод отладочных сообщений
 #define postingInterval (5 * 60 * 1000) // интервал между отправками данных в миллисекундах (5 минут)
 #define SEALEVELPRESSURE_HPA (1013.25)
 
-#define I2CSDA 4 // I2C sda
-#define I2CSCL 5 // I2C scl
-#define EnkCLK 13 //  энкодер CLK
-#define EnkDT 12 //  DT
-#define EnkSW 14 //  SW
+#define I2CSDA 4  // I2C sda
+#define I2CSCL 5  // I2C scl
+#define EnkCLK 13 // энкодер CLK
+#define EnkDT 12  // DT
+#define EnkSW 14  // SW
 
 Encoder enc1(EnkCLK, EnkDT, EnkSW); // инициализация энкодера
-LiquidCrystal_I2C lcd(0x27,20,4); // инициализация дисплея
+LiquidCrystal_I2C lcd(0x27, 20, 4); // инициализация дисплея
 Adafruit_BME280 bme;
 
-const char* ssid     = "Segma-WiFi";
-const char* password = "derq5898587";
+const char *ssid = "Segma-WiFi";
+const char *password = "derq5898587";
 boolean firstRead = true, screenLight = true;
 float bmeTemp, bmeHum, bmePres, tempHistHour[24][3];
 unsigned long lastConnectionTime = 0; // время последней передачи данных
 String Hostname;                      // * имя железки - выглядит как ESPAABBCCDDEEFF т.е. ESP+mac адрес.
 
 void wifiCon()
-{ 
+{
   Hostname = "ESP" + WiFi.macAddress();
   Hostname.replace(":", "");
   WiFi.hostname(Hostname);
   lcd.clear();
-  lcd.setCursor(1,1);
+  lcd.setCursor(1, 1);
   lcd.print("Connect to WiFi...");
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  lcd.setCursor(8,3);
+  lcd.setCursor(8, 3);
   lcd.print("*OK*");
 }
 
 void bmeRead()
 { // чтение датчика BME280
-  if(firstRead) {
-    start:
-    if (!bme.begin(0X76)) {
-      lcd.setCursor(0,3);
+  if (firstRead)
+  {
+  start:
+    if (!bme.begin(0X76))
+    {
+      lcd.setCursor(0, 3);
       lcd.print("...Sensor EROR...");
-      delay(1000*60*5);
+      delay(1000 * 60 * 5);
       goto start;
-    } else {
-      lcd.setCursor(0,3);
+    }
+    else
+    {
+      lcd.setCursor(0, 3);
       lcd.print("...Sensor GOOD...");
       firstRead = false;
     }
   }
   bmeTemp = bme.readTemperature();
   bmeHum = bme.readHumidity();
-  bmePres = bme.readPressure()/133.3224;
+  bmePres = bme.readPressure() / 133.3224;
 }
 
-void displayStart() 
+void displayStart()
 {
-  lcd.init(I2CSDA,I2CSCL); // ! До этого была строка lcd.init(4,5); ..незнаю зачем..
+  lcd.init(I2CSDA, I2CSCL); // ! До этого была строка lcd.init(4,5); ..незнаю зачем..
   lcd.backlight();
-  lcd.setCursor(2,1); 
+  lcd.setCursor(2, 1);
   lcd.print("-= Meteo WiFi =-");
-  lcd.setCursor(7,2); 
+  lcd.setCursor(7, 2);
   lcd.print("v.0.4.1");
   delay(4000);
 }
@@ -83,54 +87,74 @@ void displayStart()
 void displayFirst()
 {
   lcd.clear();
-  lcd.setCursor(0,0); lcd.print("Temp:");
-  lcd.setCursor(6,0); lcd.print(bmeTemp);
-  lcd.setCursor(0,1); lcd.print("Humm:");
-  lcd.setCursor(6,1); lcd.print(bmeHum);
-  lcd.setCursor(0,2); lcd.print("Pres:");
-  lcd.setCursor(6,2); lcd.print(bmePres); 
+  lcd.setCursor(0, 0);
+  lcd.print("Temp:");
+  lcd.setCursor(6, 0);
+  lcd.print(bmeTemp);
+  lcd.setCursor(0, 1);
+  lcd.print("Humm:");
+  lcd.setCursor(6, 1);
+  lcd.print(bmeHum);
+  lcd.setCursor(0, 2);
+  lcd.print("Pres:");
+  lcd.setCursor(6, 2);
+  lcd.print(bmePres);
 }
 
 void displayLoop()
 {
-  lcd.setCursor(6,0); lcd.print("     ");
-  lcd.setCursor(6,0); lcd.print(bmeTemp);
-  lcd.setCursor(6,1); lcd.print("     ");
-  lcd.setCursor(6,1); lcd.print(bmeHum);
-  lcd.setCursor(6,2); lcd.print("     ");
-  lcd.setCursor(6,2); lcd.print(bmePres); 
+  lcd.setCursor(6, 0);
+  lcd.print("     ");
+  lcd.setCursor(6, 0);
+  lcd.print(bmeTemp);
+  lcd.setCursor(6, 1);
+  lcd.print("     ");
+  lcd.setCursor(6, 1);
+  lcd.print(bmeHum);
+  lcd.setCursor(6, 2);
+  lcd.print("     ");
+  lcd.setCursor(6, 2);
+  lcd.print(bmePres);
 }
 
 void enk() //отключение экрана
 {
-  enc1.tick();  // отработка в прерывании
-  if (!screenLight) {
-    lcd.backlight(); 
+  enc1.tick(); // отработка в прерывании
+  if (!screenLight)
+  {
+    lcd.backlight();
     screenLight = true;
-  } else {
-    lcd.noBacklight(); 
+  }
+  else
+  {
+    lcd.noBacklight();
     screenLight = false;
   }
 }
 
-void ota() 
+void ota()
 {
   ArduinoOTA.onStart([]() {
-    Serial.println("Start");  //  "Начало OTA-апдейта"
+    Serial.println("Start"); //  "Начало OTA-апдейта"
   });
   ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");  //  "Завершение OTA-апдейта"
+    Serial.println("\nEnd"); //  "Завершение OTA-апдейта"
   });
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
     Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
   });
   ArduinoOTA.onError([](ota_error_t error) {
     Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed"); // "Ошибка при аутентификации"
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed"); // "Ошибка при начале OTA-апдейта"
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed"); // "Ошибка при подключении"
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed"); // "Ошибка при получении данных"
-    else if (error == OTA_END_ERROR) Serial.println("End Failed"); // "Ошибка при завершении OTA-апдейта"
+    if (error == OTA_AUTH_ERROR)
+      Serial.println("Auth Failed"); // "Ошибка при аутентификации"
+    else if (error == OTA_BEGIN_ERROR)
+      Serial.println("Begin Failed"); // "Ошибка при начале OTA-апдейта"
+    else if (error == OTA_CONNECT_ERROR)
+      Serial.println("Connect Failed"); // "Ошибка при подключении"
+    else if (error == OTA_RECEIVE_ERROR)
+      Serial.println("Receive Failed"); // "Ошибка при получении данных"
+    else if (error == OTA_END_ERROR)
+      Serial.println("End Failed"); // "Ошибка при завершении OTA-апдейта"
   });
   ArduinoOTA.begin();
 }
@@ -158,7 +182,7 @@ bool sendToNarodmon()
   buf = buf + "#T1#" + String(bmeTemp) + "\r\n";        // температура bmp180
   buf = buf + "#H1#" + String(bmeHum) + "\r\n";         // влажность
   buf = buf + "#P1#" + String(bmePres) + "\r\n";        // Давление
-  buf = buf + "##\r\n"; // закрываем пакет
+  buf = buf + "##\r\n";                                 // закрываем пакет
 
   if (!client.connect("narodmon.ru", 8283))
   { // попытка подключения
@@ -181,7 +205,8 @@ bool sendToNarodmon()
 
 void narodmonStart()
 {
-  if (millis() - lastConnectionTime > postingInterval && sendNM){ // ! Отправка на нар.мон.
+  if (millis() - lastConnectionTime > postingInterval && sendNM)
+  { // ! Отправка на нар.мон.
     if (WiFi.status() == WL_CONNECTED)
     { // ну конечно если подключены
       if (sendToNarodmon())
@@ -205,7 +230,7 @@ void loop()
 {
   ArduinoOTA.handle();
   enc1.tick();
-  delay(1000*10);
+  delay(1000 * 10);
   bmeRead();
   displayLoop();
   sendToNarodmon();
